7d68c06ab4900c42e8f910a61676a0e50e0a93d2
Ripred100 <60947523+Ripred100@users.noreply.github.com>
2023-07-01 17:57:59 -0400

    Broke things trying Typestate impl. Reverted

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,66 @@
 pub mod ember {
-    use canvas::digital_canvas;
-    use colorgrad::{Color, CustomGradient};
+    //use canvas::digital_canvas;
+    use colorgrad::Color;
     use rand::prelude::*;
 
-    pub struct Fireplace {
+    //This structure uses state which is any parameter that implements the trait FireplaceState
+    pub struct Fireplace<State = Running> {
+        state: std::marker::PhantomData<State>,
         pub settings: Settings,
         embers: Vec<Ember>,
         pub heatmap: [[f32; 10]; 10],
     }
 
     pub struct Settings {
-        w: f32,
-        h: f32,
-        sigma: f32,
-        heat_decay: f32,
+        _w: f32,
+        _h: f32,
+        pub sigma: f32,
+        pub heat_decay: f32,
         pub g: colorgrad::Gradient,
+        pub max_embers: usize,
+    }
+
+    pub struct Starting;
+    pub struct Running;
+    pub struct Off;
+    // pub trait FireplaceState {}
+    // impl FireplaceState for Starting {}
+    // impl FireplaceState for Running {}
+    // impl FireplaceState for Off {}
+    pub enum FireplaceState {
+        Starting,
+        Running,
+        Off,
+    }
+
+    enum EmberStatusKind {
+        Active,
+        Inactive(u8),
     }
     struct Ember {
+        pub status: EmberStatusKind,
         heat: f32,
         x: f32,
         y: f32,
     }
 
-    impl Fireplace {
-        pub fn new() -> Fireplace {
+    impl Ember {
+        pub fn activate(&mut self) {
+            self.status = EmberStatusKind::Active;
+        }
+        pub fn deactivate(&mut self) {
+            let mut rng = rand::thread_rng();
+            let jitter: f32 = rng.gen();
+            self.status = EmberStatusKind::Inactive((jitter * 10.0).round() as u8);
+        }
+    }
+
+    impl Fireplace<Running> {
+        pub fn new() -> Self {
             Fireplace {
+                state: std::marker::PhantomData::<Running>,
                 settings: Settings::new(),
-                embers: (0..10).map(|_x| Ember::new()).collect(),
+                embers: (0..12).map(|_x| Ember::new()).collect(),
                 heatmap: [[0.0; 10]; 10],
             }
         }
@@ -38,11 +72,17 @@ pub mod ember {
             for (j, row) in &mut self.heatmap.iter_mut().enumerate() {
                 for (i, space) in row.iter_mut().enumerate() {
                     for ember in &mut self.embers.iter_mut() {
-                        let distance_squared =
-                            (i as f32 - ember.x).powf(2.0) + (j as f32 - ember.y).powf(2.0);
-                        *space = *space
-                            + ((ember.heat).powf(0.5) / 10.0)
-                                * (-distance_squared / (sigma + (ember.heat).powf(0.5))).exp();
+                        match ember.status {
+                            EmberStatusKind::Active => {
+                                let distance_squared =
+                                    (i as f32 - ember.x).powf(2.0) + (j as f32 - ember.y).powf(2.0);
+                                *space = *space
+                                    + ((ember.heat).powf(0.5) / 10.0)
+                                        * (-distance_squared / (sigma + (ember.heat).powf(0.5)))
+                                            .exp();
+                            }
+                            _ => {}
+                        }
                     }
                     *space = (1.0 / -(0.7 * *space + 1.0).powf(2.0)) + 1.0
                 }
@@ -53,17 +93,32 @@ pub mod ember {
             let mut rng = rand::thread_rng();
             let decay = self.settings.heat_decay;
             for ember in self.embers.iter_mut() {
-                let jitter: f32 = rng.gen();
-                let jitter2: f32 = rng.gen();
+                match ember.status {
+                    //If the ember is inactive, we check if it is due for activation. Otherwise, decrement timer
+                    EmberStatusKind::Inactive(0) => {
+                        ember.activate();
+                    }
+                    EmberStatusKind::Inactive(counter) => {
+                        ember.status = EmberStatusKind::Inactive(counter - 1);
+                    }
+                    EmberStatusKind::Active => {
+                        let jitter: f32 = rng.gen();
+                        let jitter2: f32 = rng.gen();
 
-                ember.y = ember.y - jitter;
-                ember.x = ember.x + jitter2 - 0.5;
-                ember.heat = ember.heat - decay * (ember.heat + 1.0).ln();
-                if ember.y < 0.0 || ember.heat < 0.0 {
-                    ember.y = 11.0;
-                    ember.x = 4.0 + 4.0 * (jitter - 0.5);
-                    ember.heat = 100.0 * jitter2;
-                    //continue;
+                        ember.y = ember.y - jitter;
+                        ember.x = ember.x + jitter2 - 0.5;
+                        if ember.y < 11.0 {
+                            ember.heat = ember.heat - decay * (ember.heat + 1.0).ln();
+                        }
+
+                        if ember.y < 0.0 || ember.heat < 0.0 {
+                            ember.deactivate();
+                            ember.y = 15.0;
+                            ember.x = 4.0 + 4.0 * (jitter - 0.5);
+                            ember.heat = 100.0 * jitter2;
+                            //continue;
+                        }
+                    }
                 }
             }
         }
@@ -72,10 +127,11 @@ pub mod ember {
     impl Settings {
         pub fn new() -> Settings {
             Settings {
-                w: 10.0,
-                h: 10.0,
+                _w: 10.0,
+                _h: 10.0,
                 sigma: 0.0,
-                heat_decay: 1.5,
+                heat_decay: 1.2,
+                max_embers: 10,
                 g: colorgrad::CustomGradient::new()
                     .colors(&[
                         Color::from_rgba8(0, 0, 0, 255),
@@ -98,10 +154,12 @@ pub mod ember {
             let mut rng = rand::thread_rng();
             let jitter: f32 = rng.gen();
             let jitter2: f32 = rng.gen();
+            let jitter3: f32 = rng.gen();
             Ember {
                 heat: jitter * 100.0,
                 x: jitter2 * 10.0,
                 y: 10.0,
+                status: EmberStatusKind::Inactive((jitter3 * 10.0).round() as u8),
             }
         }
     }
